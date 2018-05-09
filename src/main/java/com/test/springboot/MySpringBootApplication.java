b22da0b94d1a14680c887bfe47f1f680eba2c8a2
fengfan <1091232731@qq.com>
2018-05-09 22:32:52 +0800

    test34

--- src/main/java/com/test/springboot/MySpringBootApplication.java
+++ src/main/java/com/test/springboot/MySpringBootApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.core.Ordered;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by lenovo on 2018/5/9.
@@ -14,10 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableAutoConfiguration
-public class MySpringBootApplication implements EmbeddedServletContainerCustomizer {
-    @RequestMapping(value="/",produces = "text/plain;charset=UTF-8")
-    public String index() {
-        return "Hello Spring Boot";
+@RequestMapping(value = "/hello")
+public class MySpringBootApplication extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer {
+    @RequestMapping(value = "/two", produces = "text/plain;charset=UTF-8")
+    public String index(@PathVariable String name) {
+
+        return "Hello Spring Boot" + name;
     }
 
     public static void main(String[] args) {
@@ -27,5 +34,12 @@ public class MySpringBootApplication implements EmbeddedServletContainerCustomiz
     @Override
     public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
         configurableEmbeddedServletContainer.setPort(8081);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
     }
 }
